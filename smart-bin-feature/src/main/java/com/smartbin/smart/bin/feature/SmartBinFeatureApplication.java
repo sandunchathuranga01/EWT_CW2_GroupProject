54fc4eb78118766dfd6096cafaa459833a63454d
sandunchathuranga02 <rishiduchathuranga@gmail.com>
2024-03-06 23:09:04 +0530

    create Smart bin feature

---
+++ smart-bin-feature/src/main/java/com/smartbin/smart/bin/feature/SmartBinFeatureApplication.java
@@ -0,0 +1,13 @@
+package com.smartbin.smart.bin.feature;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartBinFeatureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartBinFeatureApplication.class, args);
+	}
+
+}
---
+++ smart-bin-feature/src/test/java/com/smartbin/smart/bin/feature/SmartBinFeatureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartbin.smart.bin.feature;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartBinFeatureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
